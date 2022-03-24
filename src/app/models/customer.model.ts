eb99ff9cf09f7a8dcbebd01dc95d517a5014a837
aparna <aparnakuls99@gmail.com>
2022-03-24 23:38:27 +0530

    first commit

---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,11 @@
+
+
+export interface Customer{
+    customer_id: string;
+    customer_name: string;
+    gender: string;
+    date_of_birth:string;
+    phone_no:string;
+    email:string;
+
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../models/customer.model';
+import { CustomersService } from '../service-signup/customers.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+title='customers';
+customers:Customer[]=[];
+customer:Customer={
+  customer_id:'',
+  customer_name:'',
+  gender:'',
+  date_of_birth:'',
+  phone_no:'',
+  email:''
 
-  constructor() { }
+}
+  constructor(private customersService:CustomersService) {
+
+   }
 
   ngOnInit(): void {
+this.getAllCustomers();
+
   }
 
+  getAllCustomers(){
+    this.customersService.getAllCustomers()
+    .subscribe(
+      response =>{
+        this.customers=response;
+        
+
+      }
+    )
+
+  }
+
+  onSubmit(){
+
+this.customersService.addCustomer(this.customer)
+.subscribe(
+  response=>{
+    console.log(response);
+    this.getAllCustomers();
+    this.customer={
+      customer_id:'',
+  customer_name:'',
+  gender:'',
+  date_of_birth:'',
+  phone_no:'',
+  email:''
+    }
+
+  }
+)
+  }
 }
